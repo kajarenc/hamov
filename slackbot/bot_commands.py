40565f03a8119b7b9cd41caf85462b273816c7f2
karen-team <karen@teamable.me>
2018-01-29 01:37:34 +0400

    add /end functionality

--- slackbot/bot_commands.py
+++ slackbot/bot_commands.py
@@ -1,6 +1,20 @@
 import json
+import requests
 import os
 from pathlib import Path
+
+
+def process_order(string):
+    tokens = string.split(',')
+    results = []
+    for token in tokens:
+        token = token.strip()
+        if '-' in token:
+            product_id, amount = token.split('-')
+        else:
+            product_id, amount = token, '1'
+        results.append((product_id, amount))
+    return results
 
 
 def get_file_name(team_id):
@@ -24,6 +38,7 @@ def handle_order(request_dict):
     trigger_id = request_dict.get('trigger_id')
     team_id = request_dict.get('team_id')
     text = request_dict.get('text')
+
     if trigger_id and team_id and text:
         file_path = get_file_name(team_id)
         if Path(file_path).exists():
@@ -31,15 +46,44 @@ def handle_order(request_dict):
             with open(file_path, 'r') as readfile:
                 data = json.loads(readfile.read())
             if data:
-                print("DATA READEDQ!!!!!!")
-                print(data)
-                data['orders'].append({
-                    'user_id': request_dict.get('user_id'),
-                    'text': request_dict.get('text')
-                })
+                orders = process_order(request_dict.get('text'))
+                for order, amount in orders:
+                    for _ in range(int(amount)):
+                        data['orders'].append({
+                            'user_id': request_dict.get('user_id'),
+                            'text': order
+                        })
 
                 with open(file_path, 'w') as outfile:
                     json.dump(data, outfile)
             return True
         else:
             return False
+
+
+def handle_end(request_dict):
+    trigger_id = request_dict.get('trigger_id')
+    team_id = request_dict.get('team_id')
+    if trigger_id and team_id:
+        file_path = get_file_name(team_id)
+        if Path(file_path).exists():
+            with open(file_path, 'r') as readfile:
+                data = json.loads(readfile.read())
+                data_for_request = {}
+                data_for_request['restaurant_url'] = data['text']
+                payload = {}
+                for order in data['orders']:
+                    if order['user_id'] not in payload:
+                        payload[order['user_id']] = [order['text']]
+                    else:
+                        payload[order['user_id']].append(order['text'])
+                data_for_request['orders'] = payload
+                data_for_request['telephone'] = '+37455362004'
+                data_for_request['delivery_address'] = '3 Moskovyan Street'
+                data_for_request['house'] = ''
+                data_for_request['apartment'] = ''
+                data_for_request['address_details'] = 'Loft'
+                data_for_request['comments'] = ''
+                print(json.dumps(data_for_request))
+                # headers = {'Content-type': 'application/json'}
+                requests.post('http://d4bb476b.ngrok.io/orders', json=data_for_request)
--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -4,13 +4,18 @@ from flask import Flask, request, make_response, render_template
 import json
 
 from flask import Flask, request, make_response, jsonify, Response
-from bot_commands import handle_start, handle_order
+from bot_commands import handle_start, handle_order, handle_end
+import secrets
 import os
 import json
 
 from slackclient import SlackClient
 
 app = Flask(__name__)
+
+SLACK_CLIENT_ID = secrets.SLACK_CLIENT_ID
+CLIENT_SECRET = secrets.CLIENT_SECRET
+VERIFICATION_TOKEN = secrets.VERIFICATION_TOKEN
 
 
 @app.route('/')
@@ -34,13 +39,30 @@ def order():
     if handle_order(data):
         return jsonify({
             "response_type": "in_channel",
-            'text': 'GOOD!'
+            'text': 'Got it!'
         })
     else:
         return jsonify({
             "response_type": "in_channel",
             'text': 'Something went wrong! :('
         })
+
+
+@app.route("/end", methods=["POST"])
+def end():
+    data = request.form.to_dict()
+    handle_end(data)
+    return jsonify({
+        "response_type": "in_channel",
+        'text': 'ok ok, your order is in progress...!'
+    })
+
+
+@app.route("/done", methods=["POST"])
+def done():
+    slack_client = SlackClient('xoxa-305197754224-305206629728-305206629808-b6fff71346fc6fed1cfe2bd9cfa79336')
+    post_message = slack_client.api_call("chat.postMessage", channel="C8ZQRAH1S", text="DONE!!!")
+    return jsonify({"text": "OK"})
 
 
 if __name__ == '__main__':
